39247b5568ed879ac3dd78e73bd971dcde9173a5
ivanDLPG <5593312@alu.murciaeduca.es>
2023-04-18 09:09:55 +0200

    main.Client-Server-ADD-Room-List-And-Search

--- frontend/src/RoomList.jsx
+++ frontend/src/RoomList.jsx
@@ -4,20 +4,29 @@ import { Container, Row, Col, Form, Button } from "react-bootstrap";
 export const RoomList = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [rooms, setRooms] = useState([]);
-
+  const backendIP = import.meta.env.VITE_SERVER_HOST;
   useEffect(() => {
-    // Aquí deberías hacer una petición a la API para obtener todas las salas
-    // Ejemplo: fetchRooms();
-    // Una vez que tengas la respuesta de la API, actualiza el estado de las salas con setRooms
-    // Ejemplo: setRooms(response.data);
+    const fetchRooms = async () => {
+      try {
+        const response = await fetch('http://172.18.0.2:8000/api/rooms/');
+        const data = await response.json();
+        setRooms(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchRooms();
   }, []);
 
-  const handleSearch = (event) => {
+  async function handleSearch(event) {
     event.preventDefault();
-    // Aquí deberías hacer una petición a la API con el término de búsqueda
-    // Ejemplo: fetchRoomsBySearchTerm(searchTerm);
-    // Una vez que tengas la respuesta de la API, actualiza el estado de las salas con setRooms
-    // Ejemplo: setRooms(response.data);
+    try {
+      const response = await fetch(`http://172.18.0.2:8000/api/rooms/search/?search=${searchTerm}`);
+      const data = await response.json();
+      setRooms(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const handleSearchTermChange = (event) => {
@@ -38,7 +47,7 @@ export const RoomList = () => {
             />
           </Col>
           <Col xs={12} md={4} lg={3}>
-            <Button variant="primary" type="submit" block>
+            <Button variant="primary" type="submit">
               Buscar
             </Button>
           </Col>
@@ -51,8 +60,9 @@ export const RoomList = () => {
             <div className="card mb-4">
               <div className="card-body">
                 <h5 className="card-title">{room.name}</h5>
-                <p className="card-text">{room.description}</p>
-                <a href={`/salas/${room.id}`} className="card-link">
+                <p className="card-text">{room.created_at}</p>
+                <p className="card-text">Jugadores: {room.players.filter(player => player.game_mode === "p").length} | Espectadores: {room.players.filter(player => player.game_mode === "s").length} </p>
+                <a href={`/salas/${room.id}`} className="btn btn-primary">
                   Ver Sala
                 </a>
               </div>

a743c9f066354791af6facdb392c5b02ff82ca42
ivanDLPG <5593312@alu.murciaeduca.es>
2023-05-22 14:53:28 +0200

    main-ADD-Login-Needed-And-Drag&Drop-Protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
-import { RoomProvider, UserProvider } from "./contexts";
 import { HomePage, RoomListPage } from "./pages";
 import { MainNavbar, Room } from "./components";
+import { UserContext } from "./contexts";
 import "./styles/rooms.css";
 import "./styles/home.css";
 import "./styles/nav.css";
@@ -10,20 +10,23 @@ import "./styles/nav.css";
 export const App = () => {
   const location = useLocation();
   const shouldRenderNavbar = !location.pathname.includes("/sala/");
+  const { user } = useContext(UserContext);
   return (
     <>
-      <UserProvider>
-        <RoomProvider>
-          {shouldRenderNavbar && <MainNavbar />}
-          <Routes>
-            <Route path="/" element={<HomePage />} />
+      {shouldRenderNavbar && <MainNavbar />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {user ? (
+          <>
             <Route path="/salas" element={<RoomListPage />} />
-            <Route path="/sala/sala-debug" element={<Room />} />
             <Route path="/sala/:roomId" element={<Room />} />
-            <Route path="/*" element={<Navigate to="/" />} />
-          </Routes>
-        </RoomProvider>
-      </UserProvider>
+          </>
+        ) : (
+          ""
+        )}
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
     </>
   );
 };
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import { RoomProvider, UserProvider } from "./contexts";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <UserProvider>
+        <RoomProvider>
+          <App />
+        </RoomProvider>
+    </UserProvider>
   </BrowserRouter>
   // </React.StrictMode>
 );
